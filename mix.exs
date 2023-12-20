950b06bb0e94c4a994000e6eaafd43ee91d9c43c
Kevin Genus <kevin@kevingenus.com>
2023-12-20 02:24:41 -0500

    adds sobelow and its conf file

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule PipelineCi.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
-      {:sobelow, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
